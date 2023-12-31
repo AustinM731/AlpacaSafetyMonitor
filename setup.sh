3b8aa9f1998288f135623588c32318001331f5f9
Austin Lankford <austin@austinlankford.com>
2023-12-31 02:02:48 -0500

    Add logic for WeatherStation in install script

--- setup.sh
+++ setup.sh
@@ -6,15 +6,8 @@ ALPACA_SERVICE_NAME="AlpacaSafetyMonitor"
 WEATHER_SERVICE_NAME="Weather"
 PYTHON_SCRIPT="app.py"
 REQUIREMENTS_FILE="requirements.txt"
-GITHUB_REPO="https://github.com/AustinM731/AlpacaFlats.git"
 ALPACA_VENV_DIR="$ALPACA_APP_DIR/venv"
 WEATHER_VENV_DIR="$WEATHER_APP_DIR/venv"
-
-# Extract Git directory name
-# GIT_DIR_NAME=$(basename -s .git "$GITHUB_REPO")
-# GIT_CLONE_DIR="$ALPACA_APP_DIR/$GIT_DIR_NAME"
-# VENV_DIR="$GIT_CLONE_DIR/venv"
-
 
 # Installation of Python and virtualenv
 apt-get update
@@ -22,17 +15,27 @@ apt-get install -y python3 python3-venv git
 
 # Create application directory and clone the repo
 mkdir -p $ALPACA_APP_DIR
+mkdir -p $WEATHER_APP_DIR
 
-# Move contents of repo to application directory
-mv Alpaca $ALPACA_APP_DIR
+# Move contents of Alpaca repo to application directory
+mv Alpaca/* $ALPACA_APP_DIR/
 
-# Set up Python virtual environment
+# Move contents of WeatherStation repo to application directory
+mv WeatherStation/* $WEATHER_APP_DIR/
+
+# Set up Python virtual environment for Alpaca
 if [ ! -d "$ALPACA_VENV_DIR" ]; then
     python3 -m venv $ALPACA_VENV_DIR
 fi
-$ALPACA_VENV_DIR/bin/pip install -r $ALPACA_APP_DIR/Alpaca/$REQUIREMENTS_FILE
+$ALPACA_VENV_DIR/bin/pip install -r $ALPACA_APP_DIR/$REQUIREMENTS_FILE
 
-# Create a systemd service file
+# Set up Python virtual environment for Weather
+if [ ! -d "$WEATHER_VENV_DIR" ]; then
+    python3 -m venv $WEATHER_VENV_DIR
+fi
+$WEATHER_VENV_DIR/bin/pip install -r $WEATHER_APP_DIR/$REQUIREMENTS_FILE
+
+# Create a systemd service file for Alpaca
 ALPACA_SERVICE_FILE="/etc/systemd/system/$ALPACA_SERVICE_NAME.service"
 echo "[Unit]
 Description=ASCOM Alpaca driver for Flat Panel Calibrator Device
@@ -40,16 +43,31 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=$ALPACA_VENV_DIR/bin/python $ALPACA_APP_DIR/Alpaca/$PYTHON_SCRIPT
+ExecStart=$ALPACA_VENV_DIR/bin/python $ALPACA_APP_DIR/$PYTHON_SCRIPT
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target" | sudo tee $ALPACA_SERVICE_FILE
 
-# Reload systemd to include new service, then start and enable it
+# Create a systemd service file for Weather
+WEATHER_SERVICE_FILE="/etc/systemd/system/$WEATHER_SERVICE_NAME.service"
+echo "[Unit]
+Description=ASCOM Weather Station Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=$WEATHER_VENV_DIR/bin/python $WEATHER_APP_DIR/$PYTHON_SCRIPT
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" | sudo tee $WEATHER_SERVICE_FILE
+
+# Reload systemd to include new services, then start and enable them
 systemctl daemon-reload
 systemctl start $ALPACA_SERVICE_NAME
 systemctl enable $ALPACA_SERVICE_NAME
+systemctl start $WEATHER_SERVICE_NAME
+systemctl enable $WEATHER_SERVICE_NAME
 
-echo "Installation complete. Service $ALPACA_SERVICE_NAME started."
-
+echo "Installation complete. Services $ALPACA_SERVICE_NAME and $WEATHER_SERVICE_NAME started."
