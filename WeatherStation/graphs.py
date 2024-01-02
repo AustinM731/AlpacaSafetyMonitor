6d89b8b7fdb4610e3a17c9f05cdc6b07a48ca773
Austin Lankford <austin@austinlankford.com>
2024-01-02 01:05:02 -0500

    Add legends back to graphs

--- WeatherStation/graphs.py
+++ WeatherStation/graphs.py
@@ -45,10 +45,20 @@ def generate_clouds_graph(df, ax):
     # Adjusted cloud colors for dark mode
     ax.fill_between(df['timestamp'], df['ambient_temp'], df['ambient_temp'] - 10, 
                     color='deepskyblue', alpha=0.5)
+    heavy_cloud_patch = mpatches.Patch(color='deepskyblue', alpha=0.5, label='Heavy Clouds')
+
     ax.fill_between(df['timestamp'], df['ambient_temp'], df['ambient_temp'] - 25, 
                     color='dodgerblue', alpha=0.3)
+    cloud_patch = mpatches.Patch(color='dodgerblue', alpha=0.3, label='Clouds')
+
     ax.fill_between(df['timestamp'], df['ambient_temp'], df['ambient_temp'] - 30, 
                     color='lightblue', alpha=0.1)
+    light_cloud_patch = mpatches.Patch(color='lightblue', alpha=0.1, label='Light Clouds')
+
+    # Create the legend with adjusted patches
+    legend = ax.legend(handles=[*ax.get_legend_handles_labels()[0], heavy_cloud_patch, cloud_patch, light_cloud_patch], 
+                       loc='upper center', bbox_to_anchor=(0.5, -0.1), 
+                       fancybox=True, shadow=True, ncol=3)
 
     set_plot_style(ax)
     ax.set_ylabel('Temperature F')
@@ -56,29 +66,29 @@ def generate_clouds_graph(df, ax):
 
 def generate_humidity_graph(df, ax):
     ax.plot(df['timestamp'], df['humidity'], label='Humidity', color='limegreen')
-
-    set_plot_style(ax)
     ax.set_ylabel('Humidity (%)')
     ax.set_title('Humidity Graph')
+    legend = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1))
+    set_plot_style(ax)
 
 def generate_air_pressure_graph(df, ax):
     ax.plot(df['timestamp'], df['pressure'], label='Air Pressure', color='fuchsia')
-
-    set_plot_style(ax)
     ax.set_ylabel('Pressure (inHg)')
     ax.set_title('Air Pressure Graph')
+    legend = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1))
+    set_plot_style(ax)
 
 def generate_boolean_graph(df, ax):
     df['safe_numeric'] = df['safe'].astype(int)
     df['rain_numeric'] = df['rain'].astype(int)
     ax.plot(df['timestamp'], df['safe_numeric'], label='Safe', color='lime', marker='o')
     ax.plot(df['timestamp'], df['rain_numeric'], label='Rain', color='aqua', marker='o')
-
-    set_plot_style(ax)
     ax.set_ylabel('Status (0=False, 1=True)')
     ax.set_title('Safe and Rain Status')
     ax.set_yticks([0, 1])
     ax.set_yticklabels(['False', 'True'])
+    legend = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1))
+    set_plot_style(ax)
 
 db_engine = create_engine(f'postgresql://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}/{db_config["dbname"]}')
 
