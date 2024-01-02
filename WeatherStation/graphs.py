1acbf2afde0a2765ffcf9024804b5bd9fdbe261a
Austin Lankford <austin@austinlankford.com>
2024-01-02 00:51:37 -0500

    Change colors of the graphs.py script to be more visable with the dark background of the frontend.

--- WeatherStation/graphs.py
+++ WeatherStation/graphs.py
@@ -20,98 +20,61 @@ db_config = {
     "port": secrets.postgres_db_port
 }
 
-import matplotlib.patches as mpatches
+def set_plot_style(ax):
+    # Set the style for each plot
+    ax.spines['bottom'].set_color('white')
+    ax.spines['top'].set_color('white')
+    ax.spines['left'].set_color('white')
+    ax.spines['right'].set_color('white')
+    ax.xaxis.label.set_color('white')
+    ax.yaxis.label.set_color('white')
+    ax.tick_params(axis='x', colors='white')
+    ax.tick_params(axis='y', colors='white')
+    ax.title.set_color('white')
+    for line in ax.get_legend().get_lines():
+        line.set_color('white')
 
 def generate_clouds_graph(df, ax):
-    ax.plot(df['timestamp'], df['ambient_temp'], label='Ambient Temperature')
-    ax.plot(df['timestamp'], df['sky_temp'], label='Sky Temperature', color='lightblue')
+    ax.plot(df['timestamp'], df['ambient_temp'], label='Ambient Temperature', color='orange')
+    ax.plot(df['timestamp'], df['sky_temp'], label='Sky Temperature', color='cyan')
 
-    # Heavy Clouds - Darker Blue
+    # Adjusted cloud colors for dark mode
     ax.fill_between(df['timestamp'], df['ambient_temp'], df['ambient_temp'] - 10, 
-                    color='blue', alpha=0.5)
-    heavy_cloud_patch = mpatches.Patch(color='blue', alpha=0.5, label='Heavy Clouds')
-
-    # General Clouds - Medium Blue
+                    color='deepskyblue', alpha=0.5)
     ax.fill_between(df['timestamp'], df['ambient_temp'], df['ambient_temp'] - 25, 
-                    color='skyblue', alpha=0.3)
-    cloud_patch = mpatches.Patch(color='skyblue', alpha=0.3, label='Clouds')
-
-    # Light Clouds - Lighter Blue
+                    color='dodgerblue', alpha=0.3)
     ax.fill_between(df['timestamp'], df['ambient_temp'], df['ambient_temp'] - 30, 
                     color='lightblue', alpha=0.1)
-    light_cloud_patch = mpatches.Patch(color='lightblue', alpha=0.1, label='Light Clouds')
 
-    # Adjusting legend to include new cloud types
-    legend = ax.legend(handles=[*ax.get_legend_handles_labels()[0], heavy_cloud_patch, cloud_patch, light_cloud_patch], 
-                       loc='upper center', bbox_to_anchor=(0.5, -0.1), 
-                       fancybox=True, shadow=True, ncol=3)
-    legend.get_frame().set_alpha(0.5)
-
+    set_plot_style(ax)
     ax.set_ylabel('Temperature F')
     ax.set_title('Clouds Graph')
 
-
-
 def generate_humidity_graph(df, ax):
-    ax.plot(df['timestamp'], df['humidity'], label='Humidity', color='green')
+    ax.plot(df['timestamp'], df['humidity'], label='Humidity', color='limegreen')
+
+    set_plot_style(ax)
     ax.set_ylabel('Humidity (%)')
     ax.set_title('Humidity Graph')
-    legend = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1))
-    legend.get_frame().set_alpha(0.5)
 
 def generate_air_pressure_graph(df, ax):
-    ax.plot(df['timestamp'], df['pressure'], label='Air Pressure', color='purple')
+    ax.plot(df['timestamp'], df['pressure'], label='Air Pressure', color='fuchsia')
+
+    set_plot_style(ax)
     ax.set_ylabel('Pressure (inHg)')
     ax.set_title('Air Pressure Graph')
-    legend = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1))
-    legend.get_frame().set_alpha(0.5)
 
 def generate_boolean_graph(df, ax):
     df['safe_numeric'] = df['safe'].astype(int)
     df['rain_numeric'] = df['rain'].astype(int)
-    ax.plot(df['timestamp'], df['safe_numeric'], label='Safe', color='green', marker='o')
-    ax.plot(df['timestamp'], df['rain_numeric'], label='Rain', color='blue', marker='o')
+    ax.plot(df['timestamp'], df['safe_numeric'], label='Safe', color='lime', marker='o')
+    ax.plot(df['timestamp'], df['rain_numeric'], label='Rain', color='aqua', marker='o')
+
+    set_plot_style(ax)
     ax.set_ylabel('Status (0=False, 1=True)')
     ax.set_title('Safe and Rain Status')
     ax.set_yticks([0, 1])
     ax.set_yticklabels(['False', 'True'])
-    legend = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1))
-    legend.get_frame().set_alpha(0.5)
-    
-# def plot_sun_moon_positions(observer_latitude, observer_longitude, ax):
-#     observer = ephem.Observer()
-#     observer.lat = str(observer_latitude)
-#     observer.lon = str(observer_longitude)
-
-#     start_date = datetime.datetime.now()
-#     end_date = start_date + datetime.timedelta(days=2)
-#     date_range = pd.date_range(start=start_date, end=end_date, freq='10T')  # data every 10 minutes
-
-#     sun_altitudes = []
-#     moon_altitudes = []
-
-#     for single_date in date_range:
-#         observer.date = single_date
-
-#         sun = ephem.Sun(observer)
-#         moon = ephem.Moon(observer)
-        
-#         sun.compute(observer)
-#         moon.compute(observer)
-        
-#         sun_altitudes.append(float(sun.alt))
-#         moon_altitudes.append(float(moon.alt))
-
-#     ax.plot(date_range, sun_altitudes, label='Sun Altitude')
-#     ax.plot(date_range, moon_altitudes, label='Moon Altitude')
-#     ax.fill_between(date_range, 0, sun_altitudes, alpha=0.1, color='yellow')
-#     ax.fill_between(date_range, 0, moon_altitudes, alpha=0.1, color='blue')
-#     ax.set_ylabel('Altitude (radians)')
-#     ax.set_title('Sun and Moon Altitude')
-#     ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1))
-#     ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-#     ax.xaxis.set_major_locator(mdates.HourLocator(interval=12))
-
 
 db_engine = create_engine(f'postgresql://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}/{db_config["dbname"]}')
 
@@ -126,7 +89,6 @@ def generate_all_graphs(observer_latitude, observer_longitude):
     generate_humidity_graph(df, axs[1])
     generate_air_pressure_graph(df, axs[2])
     generate_boolean_graph(df, axs[3])
-    # plot_sun_moon_positions(observer_latitude, observer_longitude, axs[4])
 
     plt.tight_layout()
     current_script_path = os.path.dirname(os.path.abspath(__file__))
@@ -136,4 +98,3 @@ def generate_all_graphs(observer_latitude, observer_longitude):
     plt.close(fig)
 
 generate_all_graphs(secrets.observer_latitude, secrets.observer_longitude)
-
