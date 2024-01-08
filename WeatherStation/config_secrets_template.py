80bdbab5ba54cf6cf14c6ed7dcb0a1fa8e497ff7
Austin Lankford <austin@austinlankford.com>
2024-01-07 19:01:07 -0500

    Update secrets template to include vars needed for Mongo

--- WeatherStation/config_secrets_template.py
+++ WeatherStation/config_secrets_template.py
@@ -6,5 +6,13 @@ postgres_db_port = '<database port>'
 postgres_db_user = '<database user>'
 postgres_db_password = '<database password>'
 
+mongodb_dbname = '<database name>'
+mongodb_collection = '<collection name>'
+mongodb_host = ['<node1:port>', '<node2:port>', '<node3:port>']
+mongodb_port = '27017'
+mongodb_user = '<database user>'
+mongodb_password = '<database password>'
+mongodb_replicaset = '<replica set name>'
+
 observer_latitude = 0 #latitude in decimal degrees
 observer_longitude = 0 #longitude in decimal degrees
